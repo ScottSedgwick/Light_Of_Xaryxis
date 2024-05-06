4c051a0d93c27767410acb6c1a18ad3b2cbf3b81
Scott Sedgwick <scott.sedgwick@gmail.com>
2024-05-06 18:45:13 +1000

    Session 14

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,13 +2,16 @@ task :pages do
     sh "jekyll build -s docs -d pages --incremental"
 end
 
+desc "Serve"
 task :serve do
     sh "jekyll serve -s docs -d pages --watch --incremental"
 end
 
+desc "Clean"
 task :clean do
     sh "jekyll clean -s docs -d pages"
 end
 
+desc "Build"
 task :build => [:pages]
 task :default => [:pages]
